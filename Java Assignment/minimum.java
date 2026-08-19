878b45996f36d8cbbe7b79e77b92d54e26870425
Rupesh Adikari <bcanepal219@gmail.com>
2026-08-19 18:02:26 +0000

    minimum

--- Java Assignment/minimum.java
+++ Java Assignment/minimum.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
-public class Minimu {
+public class minimum{
     public static void main(String[] args) {
-        int n1, n2, n3, max;
+        int n1, n2, n3, min;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any three diffrent numbers:");
         n1 = sc.nextInt();
         n2 = sc.nextInt();
         n3 = sc.nextInt();
-        if
-        System.out.println("Average = " + avg);
-
+        if(n1<n2 && n1<n3)
+        System.out.println("Minimum number is"+ n1);
+        else if(n2<n3 && n2<n1)
+        System.out.println("Minimum number is"+ n2);
+        else
+        System.out.println("Minimum number is"+n3 );
         sc.close();
     }
 }
