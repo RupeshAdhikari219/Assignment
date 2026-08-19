655135505655f241892ac1cc8b0bcbbf11d032eb
Rupesh Adikari <bcanepal219@gmail.com>
2026-08-19 17:56:53 +0000

    Max

---
+++ Java Assignment/Minimum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Minimu {
+    public static void main(String[] args) {
+        int n1, n2, n3, max;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any three diffrent numbers:");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        n3 = sc.nextInt();
+        if
+        System.out.println("Average = " + avg);
+
+        sc.close();
+    }
+}
---
+++ Java Assignment/max.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class max {
+    public static void main(String[] args) {
+        int n1, n2, n3, max;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any three diffrent numbers:");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        n3 = sc.nextInt();
+        max=n1;
+        if(n2>max)
+            max=n2;
+        if(n3>max)
+            max=n3;
+        System.out.println("Maximum among 3 numbers is " + max);
+        sc.close();
+    }
+}
