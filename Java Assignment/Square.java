90e4d64c3cca05fcde994458763d2dcf2de4368f
Rupesh Adikari <bcanepal219@gmail.com>
2026-08-22 11:41:23 +0000

    Square numbers

---
+++ Java Assignment/Square.java
@@ -0,0 +1,8 @@
+class Square{
+    public static void main(String[] args)
+    {
+        System.out.println("Square Numbers betweeen 1-100:");
+        for(int n=1; n*n<=100; n++)
+            System.out.println(n*n);
+    }
+}
